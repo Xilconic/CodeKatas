e4a84a55fcbdc435490ae1812ac9b944bc479eeb
xilconic <xilconic@gmail.com>
2020-07-12 19:58:32 +0200

    Test and implement for first 2 basic degenerate cases
    - Guard against null parameters
    - Behavior for empty collection

---
+++ TheKata/src/KarateChop_V1/ArrayChopper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KarateChop_V1_Tests
+{
+    public class ArrayChopper
+    {
+        public int Chop(int number, int[] numbers)
+        {
+            if(numbers is null) throw new ArgumentNullException(nameof(numbers));
+
+            return -1;
+        }
+    }
+}
---
+++ TheKata/test/KarateChop_V1_Tests/Tests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace KarateChop_V1_Tests
+{
+    public class Tests
+    {
+        private ArrayChopper chopper;
+
+        public Tests()
+        {
+            chopper = new ArrayChopper();
+        }
+
+        [Fact]
+        public void GivenCollectionOfNumberNull_WhenChopping_ThrowArgumentNullException()
+        {
+            Action call = () => chopper.Chop(default, null);
+            call.Should().Throw<ArgumentNullException>()
+                .Which.ParamName.Should().Be("numbers");
+        }
+
+        [Fact]
+        public void GivenEmptyCollectionOfNumbers_WhenChopping_AlwaysReturnMinusOne()
+        {
+            var result = chopper.Chop(default, Array.Empty<int>());
+            result.Should().Be(-1);
+        }
+    }
+}
